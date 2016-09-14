d850f66405b2ebdd43d0699671e8cc1c92ab05ba
parm530 <parmsahadeo530@gmail.com>
2016-09-14 14:23:33 -0400

    Edited columns

--- db/migrate/20160914175024_create_products.rb
+++ db/migrate/20160914175024_create_products.rb
@@ -1,8 +1,9 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :content
-      t.string :user_id
+      t.string :name
+      t.string :link
+      t.string :message
     end
   end
 end
